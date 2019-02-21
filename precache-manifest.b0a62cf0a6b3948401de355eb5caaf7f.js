bfab32e8cb37529556c6b9e11fd5d7577dec3cbd
PierreSymonPeralta <pierre@Pierres-MacBook-Pro.local>
2019-02-22 00:35:45 +0800

    Updates

---
+++ precache-manifest.b0a62cf0a6b3948401de355eb5caaf7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9affd75a343644d565691231ccf99dd2",
+    "url": "/fashion-app/static/media/filter-icon.9affd75a.svg"
+  },
+  {
+    "revision": "6c7d59c2c6f9c769e9f8",
+    "url": "/fashion-app/static/js/runtime~main.6c7d59c2.js"
+  },
+  {
+    "revision": "6e8d603b21e59b161eb0",
+    "url": "/fashion-app/static/js/main.6e8d603b.chunk.js"
+  },
+  {
+    "revision": "d0be77769111ce3a3224",
+    "url": "/fashion-app/static/js/2.d0be7776.chunk.js"
+  },
+  {
+    "revision": "6e8d603b21e59b161eb0",
+    "url": "/fashion-app/static/css/main.575fedf3.chunk.css"
+  },
+  {
+    "revision": "2744fecf264d01563796967071a9f252",
+    "url": "/fashion-app/index.html"
+  }
+];
